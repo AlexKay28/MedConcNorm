0ac0880276068b2f5e6855216be002042caf56ff
AlexKay28 <Kaygorodow@mail.ru>
2021-03-14 16:02:27 +0300

    initital commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project utilize NN approaches to normalize medical concept.',
+    author='Kaigorodov Alexander, MEPhI Moscow (2021)',
+    license='',
+)
