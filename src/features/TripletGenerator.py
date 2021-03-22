55ba1cbc36b37765e08b847656d36b457cc54b66
alexkay28 <a.kaygorodow@ipavlov.ai>
2021-03-22 08:46:48 +0300

    added gard negative faster

--- src/features/TripletGenerator.py
+++ src/features/TripletGenerator.py
@@ -5,7 +5,9 @@ from scipy.spatial import distance
 
 
 N_RANDOM_OBS = 200
-N_POTENTIAL_EL = 30
+N_POTENTIAL_EL = 5
+choose_type = 'neg_hard'
+
 
 class TripletGenerator:
     
@@ -18,7 +20,7 @@ class TripletGenerator:
         idx = random.choice(y_label)
         return X[idx]
     
-    def choose_neg(self, X, y, anchor_x, anchor_y):
+    def choose_neg_from_n(self, X, y, anchor_x, anchor_y):
         """
         choose the neg label with attention on the closest exaples
         """
@@ -39,6 +41,21 @@ class TripletGenerator:
         
         neg_y = np.random.choice(candidates, 1, p=choose_probabilities)
         return neg_y
+    
+    def choose_neg_hard(self, X, y, anchor_x, anchor_y):
+        """
+        choose the neg label with attention on the closest exaples
+        """
+        
+        X = X[y!=anchor_y]
+        y = y[y!=anchor_y]
+        indices = np.random.choice(X.shape[0], N_RANDOM_OBS)
+        X, y = X[indices], y[indices]
+        
+        y = np.array(y)
+        d = np.array([distance.euclidean(anchor_x, ex) for ex in X])
+        neg_y = y[np.argmin(d)]
+        return neg_y
 
     def get_triplet(self, X, y, n_classes=10):
         # choose random class
@@ -46,7 +63,11 @@ class TripletGenerator:
         anchor_x = self.get_observation(X, y, anchor_y)
         pos_x    = self.get_observation(X, y, anchor_y)
         
-        neg_y = self.choose_neg(X, y, anchor_x, anchor_y)
+        if choose_type=='neg_hard':
+            neg_y = self.choose_neg_hard(X, y, anchor_x, anchor_y)
+        elif choose_type=='neg_worst_rand':
+            neg_y = self.choose_neg_from_n(X, y, anchor_x, anchor_y)
+            
         neg_x = self.get_observation(X, y, neg_y)
         
         return anchor_x, pos_x, neg_x        
