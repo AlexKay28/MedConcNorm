02f24a200047a6bf716798f7a1409aa9b7c805ad
alexkay28 <a.kaygorodow@ipavlov.ai>
2021-03-24 09:48:55 +0300

    tested with vectorized data

--- src/models/base_model.py
+++ src/models/base_model.py
@@ -4,11 +4,11 @@ from keras.layers import Dense, Dropout, Flatten, Convolution2D, MaxPooling1D
 
 def base_model(sent_emb):
     model = Sequential()
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(64, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(32, activation='relu'))    
+    model.add(Dense(256, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(64, activation='relu'))    
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(sent_emb))
